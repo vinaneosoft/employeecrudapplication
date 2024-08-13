9e38866a1adecd88669c476984b235d70cb6ce0e
vinaneosoft <vinaneosoft@gmail.com>
2024-08-13 15:26:07 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-main',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent {  // u can rename class names
   title = 'employeecrudapplication';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRouteModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component' // u can rename files
+// decorator
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent   // components, pipes
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,     // modules
+    AppRouteModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],     // services
+  bootstrap: [AppComponent]  // root component
 })
 export class AppModule { }
