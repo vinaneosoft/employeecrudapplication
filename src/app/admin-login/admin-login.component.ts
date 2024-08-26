70fd69c47cf1892980cee3bb2a9d5dbb1c207a51
vinaneosoft <vinaneosoft@gmail.com>
2024-08-26 16:05:55 +0530

    routing

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -7,16 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AdminLoginComponent {
   message="";
+  loginFlag=false; // we will add this logic LoginService : service can be shared  
+  constructor(private router:Router){
+  }
   collectData(loginForm:any){
     console.log(loginForm.value);
     this.login(loginForm.value.username,loginForm.value.password);
-    
   }
-
   login(uname:string, pass:string){
+    this.loginFlag=false;
       if(uname=="admin" && pass=='neoadmin123')
        {
-          
+        // add this username in cookie
+        this.loginFlag=true;
+        this.router.navigate(['../home']);
        }
       else
         this.message="Incorrect username or password";
